b5a1bdbc839c08b29e08c3f2842e44ec8a81d6da
Tima Maslyuchenko <dudapip3@gmail.com>
2012-01-28 19:26:29 +0200

    added hours formatter

--- app.rb
+++ app.rb
@@ -4,14 +4,14 @@ require "json"
 
 require "./report"
 
-set :public_folder, File.dirname(__FILE__) + '/public'
+set :public_folder, File.dirname(__FILE__) + "/public"
 
 get "/" do
   slim :index
 end
 
 get "/feed" do
-  abort("Wrong params") if params['start'].empty? || params['end'].empty?
+  abort("Wrong params") if params["start"].empty? || params["end"].empty?
   
   content_type :json
   
@@ -21,7 +21,7 @@ get "/feed" do
 end
 
 get "/total" do
-  abort("Wrong params") if params['month'].empty? || params['year'].empty?
+  abort("Wrong params") if params["month"].empty? || params["year"].empty?
   
   content_type :json
   
--- report.rb
+++ report.rb
@@ -6,47 +6,49 @@ class Report
     file = "config.yaml"
     @config = Psych.load_file(file)
     
-    abort('Wrong params') if !@config.is_a?(Hash) || @config['domain'].nil? || @config['username'].nil? 
+    abort('Wrong params') if !@config.is_a?(Hash) || @config["domain"].nil? || @config["username"].nil? 
   end
   
   def connect!
     Basecamp.establish_connection!(
-      @config['domain'], 
-      @config['username'], 
-      @config['password'] || 'X', 
-      @config['use_ssl'] || true, 
+      @config["domain"], 
+      @config["username"], 
+      @config["password"] || 'X', 
+      @config["use_ssl"] || true, 
     )
   end
   
   def report(from, to)
     Basecamp::TimeEntry.report({
-      'filter_project_id' => @config['project'],
-      'from' => from,
-      'to' => to
+      "filter_project_id" => @config['project'],
+      "from" => from,
+      "to" => to
     })
   end
   
   def feed(params)
     connect!
     
-    from = Time.at(params['start'].to_i).strftime('%Y%m%d')
-    to = Time.at(params['end'].to_i).strftime('%Y%m%d')
+    from = Time.at(params["start"].to_i).strftime('%Y%m%d')
+    to = Time.at(params["end"].to_i).strftime('%Y%m%d')
 
     res = {}
     report(from, to).each do |time_entry|
       key = time_entry.date.to_time.to_i.to_s + time_entry.person_id.to_s
       
       if res.has_key? key
-        res[key][:hours] += time_entry.hours.to_i 
-        res[key][:title] = "%s (%s hours)" % [time_entry.person_name, res[key][:hours]] 
+        res[key]["hours"] += time_entry.hours 
+        res[key]["title"] = "%s (%s hours)" % [time_entry.person_name, 
+          self.class.format_hours(res[key][:hours])] 
       else
         res[key] = {
-          :title => "%s (%s hours)" % [time_entry.person_name, time_entry.hours.to_i],
-          :hours => time_entry.hours.to_i,
-          :allDay => true,
-          :start => time_entry.date.to_time.to_i,
-          :end => time_entry.date.to_time.to_i,
-          :className => 'timeEntryFor' + time_entry.person_name.gsub(/\s+/, '')
+          "title" => "%s - %s" % [time_entry.person_name, 
+            self.class.format_hours(time_entry.hours)],
+          "hours" => time_entry.hours,
+          "allDay" => true,
+          "start" => time_entry.date.to_time.to_i,
+          "end" => time_entry.date.to_time.to_i,
+          "className" => 'timeEntryFor' + time_entry.person_name.gsub(/\s+/, '')
         }
       end
     end
@@ -57,22 +59,38 @@ class Report
   def total(params)
     connect!
     
-    from = Date.civil(params['year'].to_i, params['month'].to_i, 1).strftime('%Y%m%d')
-    to = Date.civil(params['year'].to_i, params['month'].to_i, -1).strftime('%Y%m%d')
+    from = Date.civil(params["year"].to_i, params["month"].to_i, 1).strftime('%Y%m%d')
+    to = Date.civil(params["year"].to_i, params["month"].to_i, -1).strftime('%Y%m%d')
     
     res = {}
+    total = 0
     report(from, to).each do |time_entry|
+      total += time_entry.hours
       if res.has_key? time_entry.person_id
-        res[time_entry.person_id]['hours'] += time_entry.hours.to_i
+        res[time_entry.person_id]['raw_hours'] += time_entry.hours
+        res[time_entry.person_id]['hours'] = self.class.format_hours(res[time_entry.person_id]['raw_hours'])
       else
         res[time_entry.person_id] = {
-          'name' => time_entry.person_name,
-          'hours' => time_entry.hours.to_i,
-          'className' => 'timeEntryFor' + time_entry.person_name.gsub(/\s+/, '')
+          "name" => time_entry.person_name,
+          "hours" => self.class.format_hours(time_entry.hours),
+          "raw_hours" => time_entry.hours,
+          "className" => 'timeEntryFor' + time_entry.person_name.gsub(/\s+/, '')
         }
       end
     end
     
-    res
+    {"summary" => res, "total" => self.class.format_hours(total)}
+  end
+  
+  class << self
+    def format_hours(hours)
+      h = hours.to_i
+      m = ((hours - h) * 60).to_i
+      
+      res = "%dh" % [h]
+      res += " %dm" % [m] if m > 0
+      
+      res
+    end
   end
 end
